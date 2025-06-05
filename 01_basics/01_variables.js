42680281143038b2c66fa3eee4256339c41f4593
AB-2458 <147861427+AB-2458@users.noreply.github.com>
2025-06-05 12:43:38 +0000

    02_datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId = 1289
+let accEmail = "Ab@chads.com"
+var accType = "Savings"
+
+// accId =3211
+accEmail="xyz@ipl.com"
+accType="Curreent"
+
+console.table([accId, accEmail, accType])
+
+/*
+Prefer not to use var as it function-scoped
+
+Use let more as it modern and block scoped
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict";//Treat all JS code as newer version
+
+//alert(3+3) , We are using nodejs not browser, that why it gives error
+
+/*Data Types:
+
+Number => 2,3,7876,
+
+String => "name","email"
+
+boolean => true or false
+
+null => standalone value
+
+undefined => 
+
+symbola => unique
+
+^ primtive dt
+
+
+object
+
+*/
+
+let age =10
+let name ="ab";
+let value=null
+
+console.log([typeof age]);
+console.log([typeof name]);
+console.log([typeof value]);
+console.log([typeof undefined]); //undefined
+console.log([typeof null]);//object
